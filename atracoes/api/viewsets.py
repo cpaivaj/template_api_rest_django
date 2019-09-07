d5ce3937d33c240dc9cf4649c890511b3cab210c
Carlos Paiva <carlos.alberto.2012@hotmail.com>
2019-09-07 11:08:50 -0300

    Usando django-filter

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -6,3 +6,8 @@ from .serializers import AtracaoSerializer
 class AtracaoViewSet(ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
+
+    ###### DJANGO FILTERS ######
+    # pip install django-filters
+    # Precisa add parametro no settings em instaled apps e ao final do arquivo settings
+    filter_fields = ('nome', 'descricao')
