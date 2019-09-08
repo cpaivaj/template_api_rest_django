a5b92d019ee8b28a6aff547e2541dc06f229dc77
Carlos Paiva <carlos.alberto.2012@hotmail.com>
2019-09-08 10:31:34 -0300

    Exibindo objetos referenciados por outros objetos

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer
+from enderecos.api.serializers import EnderecoSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
+	# NestedRelationShip Carrega os objetos internos
+    atracoes = AtracaoSerializer(many=True)
+    endereco = EnderecoSerializer()
+
     class Meta:
         model = PontoTuristico
-        fields = ['id', 'nome', 'descricao', 'aprovado', 'foto']
+        fields = [
+        	'id', 'nome', 'descricao', 'aprovado', 'foto',
+        	'atracoes', 'comentarios', 'avaliacoes', 'endereco'
+        ]
