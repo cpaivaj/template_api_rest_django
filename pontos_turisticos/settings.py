e092bc2402572bd9a324e94ac2dc9198e96dfa9c
Carlos Paiva <carlos.alberto.2012@hotmail.com>
2019-09-09 22:45:14 -0300

    databases

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'pontos_turisticos.wsgi.application'
 from dj_database_url import parse as dburl
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASE = {
+DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
