cd5c3d75656bd4c973192ca0e3cecdbf92a2fd57
Carlos Paiva <carlos.alberto.2012@hotmail.com>
2019-09-10 21:21:03 -0300

    Salvando objetos relacionados One to One

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.fields import SerializerMethodField
-from core.models import PontoTuristico, Atracao
+from core.models import PontoTuristico, Atracao#, DocIdentificacao
 from enderecos.models import Endereco
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
@@ -10,6 +10,7 @@ class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
     endereco = EnderecoSerializer()
     descricao_completa = SerializerMethodField()
+    #doc_identificacao
 
     class Meta:
         model = PontoTuristico
@@ -38,11 +39,16 @@ class PontoTuristicoSerializer(ModelSerializer):
     	endereco = validated_data['endereco']
     	del validated_data['endereco']
 
+    	#doc = validated_data['doc_identificacao']
+    	#del validated_data['doc_identificacao']
+    	#doci = DocIdentificacao.objects.create(**doc)
+
     	ponto = PontoTuristico.objects.create(**validated_data)
     	self.cria_atracoes(atracoes, ponto)
 
     	end = Endereco.objects.create(**endereco)
     	ponto.endereco = end
+    	#ponto.doc_identificacao = doci
 
     	ponto.save()
 
